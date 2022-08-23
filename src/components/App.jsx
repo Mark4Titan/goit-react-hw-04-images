836125e0c725801df5d95d3f701716f5a6e4f8f4
Mark <markfamiliytitan@gmail.com>
2022-08-24 00:06:07 +0300

    fixed the homepage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+import { RenrerSearchbar } from './Searchbar/RenrerSearchbar';
+
+export class App extends Component {
+  render() {
+    return (
+      <>
+        <RenrerSearchbar />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Searchbar/RenrerSearchbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Box } from 'components/Box';
+
+export class RenrerSearchbar extends Component { 
+
+  render() {
+
+    return (
+      <>
+        <Box p={4} m={3} display="grid" border="1px solid" width="320px">
+          <h2>щось</h2>       
+        </Box>
+      </>
+    );
+  }
+}
+
+export default RenrerSearchbar;
+
+
